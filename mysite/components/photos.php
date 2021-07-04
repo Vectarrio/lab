f6b1c11174e65e5d12a5567553d1e338d1776794
Victor Afanasenkov <vectarrio@gmail.com>
2021-07-04 19:16:26 +0300

    Пятая лаба

---
+++ mysite/components/photos.php
@@ -0,0 +1,19 @@
+<section class="photos container">
+  <h2 class="section-title">Фотографии моей кошки</h2>
+  <figure>
+    <img src="img/mphoto1.jpg" alt="Она обожает науку">
+    <figcaption>Она обожает науку</figcaption>
+  </figure>
+  <figure>
+    <img src="img/mphoto2.jpg" alt="В том числе электронику">
+    <figcaption>В том числе электронику</figcaption>
+  </figure>
+  <figure>
+    <img src="img/mphoto3.jpg" alt="И людей. Обычно.">
+    <figcaption>И людей. Обычно.</figcaption>
+  </figure>
+  <figure>
+    <img src="img/mphoto4.jpg" alt="ОБЫЧНО.">
+    <figcaption>ОБЫЧНО.</figcaption>
+  </figure>
+</section>
---
+++ mysite/index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Сайт полоскуна';
+$name = 'Неопоссумный енот';
+$image = 'img/another-raccoon.png';
+$email = 'neopossumnyienot@gmail.com';
+$phone = '+79876543210';
+
+require('components/header.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/status.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
+?>
