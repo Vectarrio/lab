f50031956f05d01c5a7407faab9e1394ec3b951e
Victor Afanasenkov <vectarrio@gmail.com>
2021-07-05 22:47:33 +0300

    Лаб 10

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
-const myskills = [
-    {name: 'HTML', value: 60, class: "skill-html"},
-    {name: 'CSS', value: 40, class: "skill-css"},
-    {name: 'C', value: 75, class: "skill-c"},
-    {name: 'Java', value: 50, class: "skill-java"},
-    {name: 'Assembler (x86/64)', value: 30, class: "skill-asm"}
-];
+fetch('db/skills.json')
+  .then(data => data.json())
+  .then(json => { 
+  	listskills(json); 
+  })
+  .catch(() => console.error("упс, что-то пошло не так"));
 const list = document.querySelector(".skills-list");
-myskills.forEach((skill) =>{
+function listskills(skills) {
+skills.forEach((skill) =>{
     const listitem = document.createElement('dt');
     const listdesc = document.createElement('dd');
     const skilllevel = document.createElement('div');
@@ -20,3 +20,4 @@ myskills.forEach((skill) =>{
     list.append(listitem);
     list.append(listdesc);
 });
+}
